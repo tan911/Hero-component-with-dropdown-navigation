0c4e1c24d015010fa5343deeb3eb6aad08124ffb
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2022-12-12 11:44:40 +0800

    working with a11y

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const buttons = document.querySelectorAll('.menu__button');
+const subButtons = document.querySelectorAll('.menu__item__submenu');
+const items = document.querySelectorAll('.menu__item');
+const header = document.querySelector('.header');
 
-btnNavEl.addEventListener('click', () => {
-  headerEl.classList.toggle('nav-open');
-  overlay.classList.toggle('hidden');
-});
+const showMenu = () => {
+  const submenu = document.querySelector('.menu__sublist')[0];
+  submenu.style.display = 'block';
+};
+
+const hideMenu = () => {
+  const submenu = document.querySelector('.menu__sublist')[0];
+  submenu.style.display = 'none';
+};
+
+for (const btn of buttons) {
+  btn.addEventListener('click', () => {
+    header.classList.toggle('menu__open');
+  });
+}
+
+for (let i = 0; i < subButtons.length; i++) {
+  subButtons[i].addEventListener('click', () => {
+    items[i].classList.toggle('menu__sub__open');
+  });
+}
