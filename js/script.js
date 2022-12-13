2db433f05e89f24509edefcab9e6597c2f4945f0
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2022-12-13 12:08:07 +0800

    add functionality

--- js/script.js
+++ js/script.js
@@ -2,25 +2,36 @@ const buttons = document.querySelectorAll('.menu__button');
 const subButtons = document.querySelectorAll('.menu__item__submenu');
 const items = document.querySelectorAll('.menu__item');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const submenu = document.querySelectorAll('.menu__sublist');
+const arrow = document.querySelectorAll('path');
 
-const showMenu = () => {
-  const submenu = document.querySelector('.menu__sublist')[0];
-  submenu.style.display = 'block';
+const showMenu = i => {
+  submenu[i].style.display = 'block';
 };
 
-const hideMenu = () => {
-  const submenu = document.querySelector('.menu__sublist')[0];
-  submenu.style.display = 'none';
+const hideMenu = i => {
+  submenu[i].style.display = 'none';
 };
 
 for (const btn of buttons) {
   btn.addEventListener('click', () => {
     header.classList.toggle('menu__open');
+    overlay.classList.toggle('hidden');
   });
 }
 
-for (let i = 0; i < subButtons.length; i++) {
-  subButtons[i].addEventListener('click', () => {
-    items[i].classList.toggle('menu__sub__open');
-  });
+if (screen.width <= 600) {
+  for (let i = 0; i < subButtons.length; i++) {
+    subButtons[i].addEventListener('click', () => {
+      items[i].classList.toggle('menu__sub__open');
+      arrow[i].d = 'm1 5 4-4 4 4';
+    });
+  }
+} else if (screen.width > 600) {
+  for (let i = 0; i < subButtons.length; i++) {
+    const submenu = document.querySelectorAll('.menu__sublist')[i];
+    items[i].onmouseenter = () => showMenu(i);
+    submenu.onmouseleave = () => hideMenu(i);
+  }
 }
